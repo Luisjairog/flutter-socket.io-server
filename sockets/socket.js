b1b8aef4b03dbe9cf51bdd785925be9e0bcc03ff
Luis <luisjairog@gmail.com>
2021-04-06 11:31:30 -0500

    seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,18 @@
+//mensaje de sockets
+
+const { io }= require('../index')
+
+io.on('connection', client => {
+    console.log('Cliente Conectado');
+   
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje',( payload )=>{
+        console.log('Mensaje', payload);
+
+        io.emit('mensaje', {admin: 'Nuevo mensaje'});
+    });
+    
+  });
